3c955bb88eda40c41508eeed71d260c774c6d971
kasselTrankos <alvaro.touzon@gmail.com>
2016-02-07 23:13:44 +0100

    init with angular

--- src/app/app.js
+++ src/app/app.js
@@ -1,11 +1,13 @@
 define(['angular'], function (angular) {
   console.log('lo ado is real');
-  function init(){
-    console.log(' initializado : : : : :');
-  }
-  var app = angular.module('ats.main', []);
+
+  var app = angular.module('ats.main', [])
+  .controller('WelcomeController', function($scope) {
+      $scope.greeting = 'Welcome!';
+  });
 
   app.init = function () {
+    console.log(' init ', document);
     angular.bootstrap(document, ['ats.main']);
   };
 
